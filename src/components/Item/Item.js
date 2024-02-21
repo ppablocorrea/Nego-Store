4c35c15e65bb4e4fe60caf8453bffb309cc2c771
Pablo Correa <pcorrea@GXC.local>
2024-02-20 21:13:27 -0300

    Cambios en las cards de los items

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,26 +1,24 @@
 import './Item.css';
 import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 
 const Item = ({id, name, img, price}) => {
     return (
-        <article className='item'>
-            <picture>
-                <Link to={`/Item/${id}`}>
-                    <img className='item-imagen' src={img} alt={name}/>
+        <Card className='Cards' style={{ width: '24rem' } }>
+            <Link to={`/Item/${id}`}>
+                <Card.Img className='imagen-Card' variant="top" src={img} alt={name}/>  
+            </Link>  
+            <Card.Body>
+                <Link className='titulo-Card' to={`/Item/${id}`}>
+                    <Card.Title>{name}</Card.Title>
                 </Link>
-            </picture>
-            
-            <header>
-                <Link to={`/Item/${id}`} className={'titulo-item'}>
-                    <h2 className='nombre-item'>{name}</h2>
-                </Link>
-            </header>
-            
-            <section>
-                <p className='precio-item'>UYU ${price}</p>
-            </section>
-        </article>
+                <Card.Text>
+                    UYU ${price}
+                </Card.Text>
+            </Card.Body>
+        </Card>
     )
 }
 
