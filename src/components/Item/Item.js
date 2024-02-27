73978022676fd74405e7283d5770a8fb3f7e0c61
Pablo Correa <pcorrea@GXC.local>
2024-02-27 19:49:18 -0300

    Estilos para cuando el stock está agotado

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 
 
-const Item = ({id, name, img, price, previousprice}) => {
+const Item = ({id, name, img, price, previousprice, stock}) => {
     return (
         <Card className='Cards' style={{ width: '24rem' } }>
             <Link to={`/Item/${id}`}>
                 <Card.Img className='imagen-Card' variant="top" src={img} alt={name}/>  
             </Link>  
             <Card.Body>
+                <p className={(stock === 0) ? 'textoAgotado' : 'ocultarTexto'}>Agotado</p>
                 <Link className='titulo-Card' to={`/Item/${id}`}>
                     <Card.Title>{name}</Card.Title>
                 </Link>
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -5,7 +5,7 @@ import Item from '../Item/Item';
 const ItemList = ({products}) => {
     return (
         <div className='lista-items'>
-            {products.map(prod => <Item key={prod.id} id={prod.id} name={prod.name} img={prod.img} price={prod.price} previousprice={prod.previousprice}/>)}
+            {products.map(prod => <Item key={prod.id} id={prod.id} name={prod.name} img={prod.img} price={prod.price} previousprice={prod.previousprice} stock={prod.stock}/>)}
         </div>
     )
 }
