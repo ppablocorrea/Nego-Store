b748e43fca994cb5dd25931bb703a227f2bd440c
Pablo Correa <pcorrea@GXC.local>
2024-12-16 15:30:48 -0300

    Cambios en el nombre

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,32 +1,45 @@
-import './NavBar.css';
-import CartWidget from '../CartWidget/CartWidget';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
-
+import "./NavBar.css";
+import CartWidget from "../CartWidget/CartWidget";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-    return (
-        <Navbar expand="lg" className="navbarBg">
-          <Container>
-            <Navbar.Brand><Link className='navbar-titulo' to='/'>Nego Store</Link></Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ms-auto">
-                <Link className='navbar-opciones' to='/Categoria/Remeras'>Remeras</Link>
-                <Link className='navbar-opciones' to='/Categoria/Buzos'>Buzos</Link>
-                <Link className='navbar-opciones' to='/Categoria/Gorras'>Gorras</Link>
-                <Link className='navbar-opciones' to='/Categoria/Jeans'>Jeans</Link>
-                <Link className='navbar-opciones ultima' to='/Categoria/Coleccion'>Coleccion</Link>
-              </Nav>
-              <Nav className="ms-auto carritocentrado">
-                <CartWidget/>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-    )
-}
+  return (
+    <Navbar expand="lg" className="navbarBg">
+      <Container>
+        <Navbar.Brand>
+          <Link className="navbar-titulo" to="/">
+            Ecommerce
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Link className="navbar-opciones" to="/Categoria/Remeras">
+              Remeras
+            </Link>
+            <Link className="navbar-opciones" to="/Categoria/Buzos">
+              Buzos
+            </Link>
+            <Link className="navbar-opciones" to="/Categoria/Gorras">
+              Gorras
+            </Link>
+            <Link className="navbar-opciones" to="/Categoria/Jeans">
+              Jeans
+            </Link>
+            <Link className="navbar-opciones ultima" to="/Categoria/Coleccion">
+              Coleccion
+            </Link>
+          </Nav>
+          <Nav className="ms-auto carritocentrado">
+            <CartWidget />
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default NavBar
+export default NavBar;
